db3f5515c111b0d1f4e1ecba8b1e690d9c7c073c
Nahom Bekele <nahom.fix@gmail.com>
2022-07-17 00:31:09 +0300

    perf: update distance calculator

--- admin/contracts/RefundByLocation.sol
+++ admin/contracts/RefundByLocation.sol
@@ -17,6 +17,10 @@ contract Refund {
     Location public officeLocation;
     address[] public employees;
     uint public maxBalance;
+    uint public PI = 3141592653589793238;
+    uint public EARTHRADIUS = 6371;
+    uint public LOCATIONFACTOR = 1000000;
+    uint public WEIFACTOR = 1000000000000000000;
 
     // function() public payable {}
 
@@ -49,16 +53,34 @@ contract Refund {
         return false;
     }
 
+    function sqrt(uint x) pure public returns (uint y){
+        uint z = (x + 1) / 2;
+        y = x;
+        while (z < y) {
+            y = z;
+            z = (x / z + z) / 2;
+        }
+    }
+
     // check if the location of the employee is in range
-    function isUserLocationInRange(Location memory employeeLocation) private {}
+    function isUserLocationInRange(Location memory employeeLocation) private {
+        uint lat = employeeLocation.lat - officeLocation.lat; // Difference of latitude
+        uint lon = employeeLocation.lng - officeLocation.lng; // Difference of longitude
+
+        uint disLat = (lat * PI * EARTHRADIUS) / 180; // Vertical distance
+        uint disLon = (lon * PI * EARTHRADIUS) / 180; // Horizontal distance
+
+        uint distance = sqrt((disLat)**2 + (disLon)**2); // distance in kms
+        if (distance <= (radius * WEIFACTOR)) {
+            uint payment = maxBalance / timeLimit;
+            payable(msg.sender).transfer(payment);
+        }
+    }
 
     // report the location of the employee
     function reportLocation(uint _employeeLocationLat, uint _employeeLocationLng) public {
         require(checkIfEmployeeKnown(msg.sender), "Unknown employee");
         require(msg.sender != owner, "Must not be owner");
-        uint payment = maxBalance / timeLimit;
-        payable(msg.sender).transfer(payment);
-        console.log("Lat: ", _employeeLocationLat);
-        console.log("Lng: ", _employeeLocationLng);
+        isUserLocationInRange(Location({lat: _employeeLocationLat, lng: _employeeLocationLng}));
     }
 }
