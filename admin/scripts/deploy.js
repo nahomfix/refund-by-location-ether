3126af67a0e9b6c07dfcd5db0d2e545afcacf67e
Nahom Bekele <nahom.fix@gmail.com>
2022-07-17 23:58:03 +0300

    chore: add rinkeby network in config

--- admin/scripts/deploy.js
+++ admin/scripts/deploy.js
@@ -7,6 +7,10 @@
 const hre = require("hardhat");
 
 async function main() {
+    const [deployer] = await hre.ethers.getSigners();
+
+    console.log("Deploying contracts with the account:", deployer.address);
+
     const RefundByLocation = await hre.ethers.getContractFactory(
         "RefundByLocation"
     );
